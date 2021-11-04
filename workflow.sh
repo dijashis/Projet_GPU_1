6399981513c500db68e4d091adc26be004a07dfd
DIOP Khadidiatou <81750437+dijashis@users.noreply.github.com>
2021-11-04 12:34:52 +0100

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 #cd "$(dirname "$0")"
 
-echo 'Team 4 (Universite Clermont Auvergne, Mesocentre)'
+__author__= "Diop Khadidiatou'
+
+
+echo 'Team 1 (Universite Clermont Auvergne, Mesocentre)'
 echo 'Date: Fall Master course 2021 '
 echo 'Object: Sample case of ATACseq workflow showing job execution and dependen
 cy handling.'
-echo 'Inputs: paths to scripts qc, trim and bowtie2, atac_rem_dup, atacseq_deepl, atac_macs, atac_bedtools'
+echo 'Inputs: paths to scripts qc, cutadapt and bowtie2, atac_indexing, atac_....(a changer GILLES), atacseq_deepl, atac_macs, atac_bedtools'
 echo 'Outputs: trimmed fastq files, QC HTML files and BAM files, BED files, .npz files, .tab files, .png files, (.narrowPeak, .xls, .r files)'
 
 # Handling errors
@@ -22,7 +25,7 @@ echo "Launching Atac-seq Workflow"
 jid1=$(sbatch --parsable scripts/atac_qc_init.slurm)
 echo "$jid1 : Initial Quality Control"
 
-# Trimming
+# Trimming ou cutadapt ?
 jid2=$(sbatch --parsable --dependency=afterok:$jid1 scripts/atac_trim.slurm)
 echo "$jid2 : Trimming with Trimmomatic tool"
 
@@ -31,11 +34,15 @@ jid3=$(sbatch --parsable --dependency=afterok:$jid2 scripts/atac_qc_post.slurm)
 echo "$jid3 : Post control_quality"
 
 # Bowtie2 Alignment
-jid4=$(sbatch --parsable --dependency=afterok:$jid3 scripts/atac_bowtie2.slurm)
+jid4-1=$(sbatch --parsable --dependency=afterok:$jid3 scripts/atac_bowtie2.slurm)
 echo "$jid4 : Sequence Alignment Using Bowtie2 "
 
+# indexation 
+jid4-2=$(sbatch --parsable --dependency=afterok:$jid4-1 scripts/atac_indexation.slurm)
+echo "$jid4 : Indexing of the GRCm8.p6 and GRCm39 mouse reference genomes. "
+
 # Cleaning Alignments
-jid5=$(sbatch --parsable --dependency=afterok:$jid4 scripts/atac_rem_dup.slurm)
+jid5=$(sbatch --parsable --dependency=afterok:$jid4-2 scripts/atac_rem_dup.slurm)
 echo "$jid5 : Cleaning alignments Using Picard Tools"
 
 #  Data Exploitation
@@ -50,9 +57,13 @@ echo "$jid7 : Identification of dna accessibility sites using MACS2"
 jid8=$(sbatch --parsable --dependency=afterok:$jid7 scripts/atac_bedtools)
 echo "$jid8 : Identification of common accessibility sites using BedTools"
 
+#  Visualisation( A FAIRE )
+jid9=$(sbatch --parsable --dependency=afterok:$jid7 scripts/.....)
+echo "$jid9 : Visualisations"
+
 #  Info on workflow
-jid9=$(sbatch --parsable --dependency=afterok:$jid8 scripts/)
-echo "$jid9 : Get general info from worflow"
+jid10=$(sbatch --parsable --dependency=afterok:$jid8 scripts/)
+echo "$jid10 : Get general info from worflow"
 
 
 echo "Stop job : "`date` >&2

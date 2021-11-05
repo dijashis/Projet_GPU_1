909f955ae8d561522a40005bee051ed182f6b17d
DIOP Khadidiatou <81750437+dijashis@users.noreply.github.com>
2021-11-05 12:19:21 +0100

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 #cd "$(dirname "$0")"
 
-__author__= "Diop Khadidiatou'
 
 
-echo 'Team 1 (Universite Clermont Auvergne, Mesocentre)'
+#! /bin/bash
+#cd "$(dirname "$0")"
+
+echo 'Groupe 1 : projet HPC '
 echo 'Date: Fall Master course 2021 '
-echo 'Object: Sample case of ATACseq workflow showing job execution and dependen
-cy handling.'
-echo 'Inputs: paths to scripts qc, cutadapt and bowtie2, atac_indexing, atac_....(a changer GILLES), atacseq_deepl, atac_macs, atac_bedtools'
-echo 'Outputs: trimmed fastq files, QC HTML files and BAM files, BED files, .npz files, .tab files, .png files, (.narrowPeak, .xls, .r files)'
+echo 'Object: ATAC-SEQ'
+echo 'Inputs: paths to scripts'
+echo 'Outputs: trimmed fastq files, QC HTML files, BAM and BEDfiles'
 
 # Handling errors
 #set -x # debug mode on
@@ -19,51 +20,53 @@ set -o nounset # force variable initialisation
 
 IFS=$'\n\t'
 
-echo "Launching Atac-seq Workflow"
-# first job - no dependencies
-# Initial QC
-jid1=$(sbatch --parsable scripts/atac_qc_init.slurm)
-echo "$jid1 : Initial Quality Control" 
+rm -f /home/users/"$USER"/results/performance_workflow.txt
 
-# Trimming ou cutadapt ?
-jid2=$(sbatch --parsable --dependency=afterok:$jid1 scripts/atac_trim.slurm)
-echo "$jid2 : Trimming with Trimmomatic tool"
+# first job - no dependencies
+
+# Initial QC
+jid1=$(sbatch --parsable script/atac_qc_init_Brice.slurm)
+sbatch --dependency=afterok:$jid1 /home/users/"$USER"/scripts/performance.slurm "$jid1"
+echo "$jid1 : Initial Quality Control"
+
+# Trimming with cutadapt
+jid2=$(sbatch --parsable --dependency=afterok:$jid1 script/atac_cutadapt_Brice.slurm)
+sbatch --dependency=afterok:$jid2 /home/users/"$USER"/scripts/performance.slurm "$jid2"
+echo "$jid2 : Trimming with cutadapt"
+
 
 # Post QC
-jid3=$(sbatch --parsable --dependency=afterok:$jid2 scripts/atac_qc_post.slurm)
-echo "$jid3 : Post control_quality"
+jid3=$(sbatch --parsable --dependency=afterok:$jid2 script/atac_qc_post_Brice.slurm)
+sbatch --dependency=afterok:$jid3 /home/users/"$USER"/scripts/performance.slurm "$jid3"
+echo "$jid3 : Post Quality Control"
 
-# Bowtie2 Alignment
-jid4-1=$(sbatch --parsable --dependency=afterok:$jid3 scripts/atac_bowtie2.slurm)
-echo "$jid4 : Sequence Alignment Using Bowtie2 "
-
-# indexation 
-jid4-2=$(sbatch --parsable --dependency=afterok:$jid4-1 scripts/atac_indexation.slurm)
-echo "$jid4 : Indexing of the GRCm8.p6 and GRCm39 mouse reference genomes. "
-
-# Cleaning Alignments
-jid5=$(sbatch --parsable --dependency=afterok:$jid4-2 scripts/atac_rem_dup.slurm)
-echo "$jid5 : Cleaning alignments Using Picard Tools"
-
-#  Data Exploitation
-jid6=$(sbatch --parsable --dependency=afterok:$jid5 scripts/atacseq_deepl)
-echo "$jid6 : Exploiting The Data Using DeepTools"
-
-#  Identification of dna accessibility sites
-jid7=$(sbatch --parsable --dependency=afterok:$jid6 scripts/atac_macs)
-echo "$jid7 : Identification of dna accessibility sites using MACS2"
-
-#  Identification of common accessibility sites
-jid8=$(sbatch --parsable --dependency=afterok:$jid7 scripts/atac_bedtools)
-echo "$jid8 : Identification of common accessibility sites using BedTools"
-
-#  Visualisation( A FAIRE )
-jid9=$(sbatch --parsable --dependency=afterok:$jid7 scripts/.....)
-echo "$jid9 : Visualisations"
-
-#  Info on workflow
-jid10=$(sbatch --parsable --dependency=afterok:$jid8 scripts/)
-echo "$jid10 : Get general info from worflow"
+#bowtie2-build : builds a Bowtie index from Mus_musculus_GRCm38
+jid4=$(sbatch --parsable --dependency=afterok:$jid3 script/atac_bowtie2_build_indexer.slurm)
+sbatch --dependency=afterok:$jid4 /home/users/"$USER"/scripts/performance.slurm "$jid4"
+echo "$jid4: Building a Bowtie index from Mus_musculus_GRCm38"
 
 
-echo "Stop job : "`date` >&2
+# Bowtie2 : alignment
+jid5=$(sbatch --parsable --dependency=afterok:$jid4 script/atac_bowtie2_Brice.slurm)
+sbatch --dependency=afterok:$jid5 /home/users/"$USER"/scripts/performance.slurm "$jid5"
+echo "$jid5: Alignment"
+
+# Picard.jar, MarkDuplicates : removing duplicates 
+jid6=$(sbatch --parsable --dependency=afterok:$jid5 script/atac_MarkDuplicates_Brice.slurm)
+sbatch --dependency=afterok:$jid6 /home/users/"$USER"/scripts/performance.slurm "$jid6"
+echo "$jid6: Removing duplicates with Picard Tool MarkDuplicates"
+
+# DeepTools - User-friendly tools for exploring deep-sequencing data; BAM files analysis
+jid7=$(sbatch --parsable --dependency=afterok:$jid6 script/atac_deepTools_Brice.slurm)
+sbatch --dependency=afterok:$jid7 /home/users/"$USER"/scripts/performance.slurm "$jid7"
+echo "$jid7: DeepTools : BAM files analysis"
+
+# MACS2 Peak Calling : dna accessibility sites
+jid8=$(sbatch --parsable --dependency=afterok:$jid7 script/atac_Macs2_Brice.slurm)
+sbatch --dependency=afterok:$jid8 /home/users/"$USER"/scripts/performance.slurm "$jid8"
+echo "$jid8: MACS2 Peak Calling : dna accessibility sites."
+
+# Bedtools intersect ; searching for uniques and common accessibility sites between the different cellular stages
+jid9=$(sbatch --parsable --dependency=afterok:$jid8 script/atac_bedTools_Brice.slurm)
+sbatch --dependency=afterok:$jid9 /home/users/"$USER"/scripts/performance.slurm "$jid9"
+echo "$jid9 : Bedtools intersect ; searching for uniques and common accessibility sites between the different cellular stages."

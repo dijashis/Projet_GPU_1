93f78f4c73edb63a1593f5911e52f5c561996f02
DIOP Khadidiatou <81750437+dijashis@users.noreply.github.com>
2021-11-05 18:57:46 +0100

    Delete sacct.sh

--- sacct.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=0:40:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem=2G
-#SBATCH -o log/slurmjob-%A-%a
-#SBATCH --job-name=Sacct
-#SBATCH --partition=short
-sacct --format=jobid,elapsed,ncpus,AllocCPUS,CPUTime,TotalCPU,ReqMem,ntasks,state,User > Info_workflow.txt
